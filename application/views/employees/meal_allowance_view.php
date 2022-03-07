8e74533756740f99d1e3c5a8402a966485c38dc1
Paul <paulalcabasa@gmail.com>
2022-03-07 23:10:13 +0800

    Meal allowance module

--- application/views/employees/meal_allowance_view.php
+++ application/views/employees/meal_allowance_view.php
@@ -4,8 +4,11 @@
             <small>You can reload meal allowance of employees in this page</small>
         </h1>    
     </section>
+    
     <section class="content"> <!-- Main content -->
         <p class="alert alert-info">Remove the persons to exclude from the meal allowance, not checking anyone will reset all alowances of all the employees.</p>
+        <p class="alert alert-success" v-if="submitFlag">Meal allowances has been successfully updated.</p>
+        
         <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
                 <li v-bind:class="department_id == row.id ? 'active' : ''" v-for="(row, index) in departments">
@@ -54,152 +57,43 @@
           
          
     </section> <!-- /.content -->
-</div><!-- /.content-wrapper -->
 
+<div class="modal fade" id="modalConfirm" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure to submit the meal allowance details?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" @click="updateMealAllowance()">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div><!-- /.content-wrapper -->
 <script src="../assets/js/vue.js"></script>
 <script>
-
-var $idown;  // Keep it outside of the function, so it's initialized once.
-var table;
-function load_employees(department_id){
-   $.ajax({
-        type:"POST",
-        data:{
-            department_id : department_id
-        },
-        url:"ajax_get_employees_by_department2",
-        success:function(response){
-            $("#employees_list tbody").html(response);
-            table =  $('#employees_list').DataTable( {
-                "scrollY":        "350px",
-                "scrollCollapse": true,
-                "paging":         false,
-                "searching" : true,
-                "ordering" : false,
-                "info" : false
-            });      
-        }
-    });
-}
-function downloadURL(url) {
-    if ($idown) {
-        $idown.attr('src',url);
-    } else {
-        $idown = $('<iframe>', { id:'idown', src:url }).hide().appendTo('body');
-    }
-}
-$(document).ready(function(){
-    
-    
-  //  load_employees(department_id);
-    $("#btn_trigger_xls").click(function(){
-        $("#ma_xls").click();
-    });
-
-    $("#ma_xls").change(function(){
-        $("#frm_ma_xls").submit();
-    });
-   
-    // $("body").on("click",".btn_get_employees",function(){
-    //     var department_id = $(this).data('department_id');
-    //     table.destroy();
-    //     load_employees(department_id);
-    // });
-
-    $("#cb_main").click(function(){
-        if($(this).is(":checked")){
-            $(".cb_employee").prop("checked",true);
-        }
-        else {
-           $(".cb_employee").prop("checked",false);
-        }
-    });
-
-    $("#btn_download_list").click(function(){
-        var selected_employees = [];
-        var index = 0;
-         $("#btn_download_exported_employees").click();
-        $('.cb_employee').each(function(){
-            if($(this).is(":checked")){
-                selected_employees[index] = $(this).data('person_id');
-                index++;
-            }
-        });
-
-
-        if(index > 0){
-            $("#selected_employees").val(selected_employees);
-           /*var formData = new FormData($("#frm_selected_employees")[0]);
-           formData.append("selected_employees", selected_employees);*/
-
-           $("#frm_selected_employees").submit();
-            /*$.ajax({
-                type:"POST",
-                data:{
-                    selected_employees : selected_employees
-                },
-                url:"ajax_export_selected_employees_ma",
-                success:function(response){
-                    alert(response);
-                   //... How to use it:
-                 //  alert("<?php echo base_url();?>files/exported_employees/ma_employees.xls");
-                 //   downloadURL('<?php echo base_url();?>files/exported_employees/ma_employees.xls');
-                }
-            });*/
-        }
-        else {
-            alert('Kindly select employees to download.');
-        }
-    });
-
-   
-
-    $('.start_date, .end_date').datetimepicker({
-        "showDropdowns": true,
-        "showWeekNumbers": true,
-        singleDatePicker: true,
-    });
-
-    $("#txt_validity_date").val("");
-
-    $('#txt_validity_date').on('apply.daterangepicker', function(ev, picker) {
-        start_date = picker.startDate.format('YYYY-MM-DD');
-        end_date = picker.endDate.format('YYYY-MM-DD');
-        $("#txt_valid_from").val(start_date);
-        $("#txt_valid_until").val(end_date);
-    });
-        
-    $("#btn_reload_ma").click(function(){
-        if($("#txt_valid_from").val() == "" || $("#txt_valid_until").val() == ""){
-            $("#msg").html('Kindly select validity date.');
-        }
-        else if($("#ma_xls").val() == ""){
-            $("#msg").html('Kindly select file.');
-        }
-        else {
-            $("#msg").html('<i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i><span style="font-size:20px;">Processing</span>');
-            $("#frm_ma_reload").submit();
-        }
-        $("#msg").show();
-    });
-
-
-    
-});
-
-
 Vue.createApp({
     data() {
         return {
             department_id : 1,
             employees: [],
             search : '',
-            departments : []
+            departments : [],
+            submitFlag : false
         }
     },
     methods : {
         loadEmployees(department_id){
             var self = this;
+            this.submitFlag = false;
             $.ajax({
                 type:"POST",
                 data:{
@@ -228,8 +122,10 @@ Vue.createApp({
             this.employees.splice(index, 1);
         },
         submit(){
+            $("#modalConfirm").modal('show');
+        },
+        updateMealAllowance(){
             var self = this;
-
             $.ajax({
                 type:"POST",
                 url:"ajax_reload_meal_allowance",
@@ -237,7 +133,8 @@ Vue.createApp({
                     employees: self.employees
                 },
                 success:function(response){
-                    console.log(response);
+                    self.submitFlag = true;
+                    $("#modalConfirm").modal('hide');
                 }
             });
         }
